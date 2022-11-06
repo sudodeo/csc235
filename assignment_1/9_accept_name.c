afa6cccf7675193f9876873a93f679198a015a36
sudodeo <adesolaadeoluwa@gmail.com>
2022-11-06 19:15:23 +0100

    add comment and new line before printing names

--- assignment_1/9_accept_name.c
+++ assignment_1/9_accept_name.c
@@ -1,16 +1,16 @@
-// accept names from 10 students and print all names
-
 #include <stdio.h>
 
 int main()
 {
     char name[10][20];
     int i;
+    // collect names from input and append to name
     for (i = 0; i < 10; i++)
     {
         printf("Input student %d name: ", i + 1);
         scanf("%s", name[i]);
     }
+    printf("\n");
     for (i = 0; i < 10; i++)
     {
         printf("%s\n", name[i]);
